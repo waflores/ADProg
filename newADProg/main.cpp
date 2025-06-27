34c6d8d005a15659bf88c3c95afefeedf0d16f14
Will Flores <waflores956@gmail.com>
2025-06-26 22:34:47 -0400

    Starting out with an example Qt6.9 QApplication - going to try uniform initialization next.

---
+++ newADProg/main.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2025-06-26
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#include <QtWidgets>
+
+int main(int argc, char **argv) {
+  QApplication app(argc, argv);
+  QWidget window;
+  
+  //! [create, lay out widgets and show]
+  QLabel *label = new QLabel(QApplication::translate("windowlayout", "Name:"));
+  QLineEdit *lineEdit = new QLineEdit();
+
+  QHBoxLayout *layout = new QHBoxLayout();
+  layout->addWidget(label);
+  layout->addWidget(lineEdit);
+  window.setLayout(layout);
+  //! [create, lay out widgets and show]
+  window.setWindowTitle(
+      QApplication::translate("windowlayout", "Window layout"));
+  window.show();
+  return app.exec();
+}
